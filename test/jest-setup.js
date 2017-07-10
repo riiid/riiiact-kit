88186d8b1ad60cc095743270854cee267e2a8acd
chitacan <chitacan@gmail.com>
2017-07-11 04:05:30 +0900

    :arrow_up: update deps.

--- test/jest-setup.js
+++ test/jest-setup.js
@@ -1,4 +1,4 @@
-import {PropTypes} from 'react';
+import PropTypes from 'prop-types';
 import injectTabEvent from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
